81a47af87b2c4e8c59211e129594d455b3609bc7
temidayo <temmideee@gmail.com>
2024-01-28 22:17:05 +0100

    jersey

--- src/components/userPlayer/UserPlayer.tsx
+++ src/components/userPlayer/UserPlayer.tsx
@@ -15,12 +15,13 @@ export default function UserPlayer({
   element_type,
   formationIndex,
 }: UserPlayerProps) {
+  //
   return (
     <div
       className="flex flex-col text-center justify-center items-center w-[70px] font-body cursor-pointer "
       onClick={() => handleSelectPosition(element_type, formationIndex)}
     >
-      <img src={image} alt="jersey" className="mb-1 w-full" />
+      <img src={image} alt="football-jersey" className="mb-1 w-full" />
       <h5 className="font-bold truncate w-full">{name}</h5>
       <span className="text-[12px] font-semibold">{team}</span>
     </div>
